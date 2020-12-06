132ec22719eb2d1296b75f8eec74d6cd8eb91466
Muhammed Shaheer <ckshaheer2012@gmail.com>
2020-12-06 18:56:12 +0530

    Basic setup

---
+++ src/main/java/com/marketplace/library/LibraryApplication.java
@@ -0,0 +1,18 @@
+package com.marketplace.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Muhammed Shaheer
+ * @since 06 December 2020
+ */
+
+@SpringBootApplication
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+
+}
